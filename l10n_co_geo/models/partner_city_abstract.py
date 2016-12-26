72a6a368aa80523c2d7cd2d4f66971c8399bb0f4
David Arnold <dar@devco.co>
2016-12-25 19:24:22 -0500

    [FIX] Company adress & synchronization
    Before this commit, company onchanges did not work orrectly as there was an additional old style onchange in the view
    Additionally the whole calculation of city_id was not correctly in line with the computed address fields which synchronize with the "contact partner" found on the related partner. In order to solve this more elegantly part of the v10 code was backported, which does not work with traditional function fields any more.
    
    Signed-off-by: David Arnold <dar@devco.co>

--- l10n_co_geo/models/partner_city_abstract.py
+++ l10n_co_geo/models/partner_city_abstract.py
@@ -10,22 +10,12 @@ class PartnerCityAbstract(models.AbstractModel):
 
     city_id = fields.Many2one('res.country.state.city', 'City')
 
-    def _enforce_address_domains(self):
-        domain = dict(city_id=[], state_id=[])
-        if self.country_id:
-            domain['state_id'] = [('country_id', '=', self.country_id.id)]
-            domain['city_id'] = [('country_id', '=', self.country_id.id)]
-        if self.state_id:
-            domain['city_id'] = [('state_id', '=', self.state_id.id)]
-        return {'domain': domain}
-
     @api.onchange('city_id')
     def _onchange_city(self):
         self.city = self.city_id.name
         if self.city_id:
             self.state_id = self.city_id.state_id
             self.country_id = self.city_id.country_id
-        return self._enforce_address_domains()
 
     @api.onchange('state_id')
     def _onchange_state(self):
@@ -33,7 +23,6 @@ class PartnerCityAbstract(models.AbstractModel):
             if self.city_id.state_id != self.state_id:
                 self.city_id = False
             self.country_id = self.state_id.country_id
-        return self._enforce_address_domains()
 
     @api.onchange('country_id')
     def _onchange_country(self):
@@ -42,7 +31,6 @@ class PartnerCityAbstract(models.AbstractModel):
                 self.city_id = False
             if self.state_id.country_id != self.country_id:
                 self.state_id = False
-        return self._enforce_address_domains()
 
     def _complete_address(self, vals):
 
--- l10n_co_geo/models/res_company.py
+++ l10n_co_geo/models/res_company.py
@@ -2,16 +2,83 @@
 # Copyright 2016 David Arnold, DevCO Colombia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from openerp import models, api
+from openerp import models, fields, api
 
 
 class ResCompany(models.Model):
     _name = 'res.company'
     _inherit = ['res.company', 'partner.city.abstract']
 
-    @api.model
-    def _address_fields(self):
-        return super(ResCompany, self)._address_fields() + ['city_id']
+    # Backport of v10
+    street = fields.Char(compute='_compute_address', inverse='_inverse_street')
+    street2 = fields.Char(compute='_compute_address',
+                          inverse='_inverse_street2')
+    zip = fields.Char(compute='_compute_address', inverse='_inverse_zip')
+    city = fields.Char(compute='_compute_address', inverse='_inverse_city')
+    state_id = fields.Many2one('res.country.state', compute='_compute_address',
+                               inverse='_inverse_state', string="Fed. State")
+    country_id = fields.Many2one(
+        'res.country', compute='_compute_address',
+        inverse='_inverse_country', string="Country")
+    fax = fields.Char(compute='_compute_address', inverse='_inverse_fax')
+    city_id = fields.Many2one(
+        'res.country.state.city', 'City',
+        compute="_compute_address", inverse='_inverse_city_id')
+
+    @api.onchange('country_id')
+    def _onchange_country_wrapper(self):
+        values = self.on_change_country(self.country_id.id)['value']
+        for fname, value in values.iteritems():
+            setattr(self, fname, value)
+
+    # TODO @api.depends(): currently now way to formulate the dependency on the
+    # partner's contact address
+    def _compute_address(self):
+        for company in self.filtered(lambda company: company.partner_id):
+            address_data = company.partner_id.sudo(
+            ).address_get(adr_pref=['contact'])
+            if address_data['contact']:
+                partner = company.partner_id.browse(address_data['contact'])
+                company.street = partner.street
+                company.street2 = partner.street2
+                company.city = partner.city
+                company.zip = partner.zip
+                company.state_id = partner.state_id
+                company.city_id = partner.city_id
+                company.country_id = partner.country_id
+                company.fax = partner.fax
+
+    def _inverse_street(self):
+        for company in self:
+            company.partner_id.street = company.street
+
+    def _inverse_street2(self):
+        for company in self:
+            company.partner_id.street2 = company.street2
+
+    def _inverse_zip(self):
+        for company in self:
+            company.partner_id.zip = company.zip
+
+    def _inverse_city(self):
+        for company in self:
+            company.partner_id.city = company.city
+
+    def _inverse_city_id(self):
+        for company in self:
+            company.partner_id.city_id = company.city_id
+
+    def _inverse_state(self):
+        for company in self:
+            company.partner_id.state_id = company.state_id
+
+    def _inverse_country(self):
+        for company in self:
+            company.partner_id.country_id = company.country_id
+
+    def _inverse_fax(self):
+        for company in self:
+            company.partner_id.fax = company.fax
 
     @api.model
     def create(self, vals):
